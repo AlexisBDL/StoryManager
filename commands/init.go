5eae567dee621f745c7064ffeb006441de680a1c
Alexis Bredel <alexis.bredel@outlook.com>
2020-04-23 16:15:19 +0200

    Init commande

---
+++ commands/init.go
@@ -0,0 +1,31 @@
+package commands
+
+import (
+	"fmt"
+
+	"github.com/attic-labs/noms/go/config"
+	"github.com/attic-labs/noms/go/d"
+
+	"github.com/spf13/cobra"
+)
+
+func runInit(cmd *cobra.Command, args []string) error {
+	cfg := config.NewResolver() //config default db "Stories"
+	db, err := cfg.GetDatabase("")
+	d.PanicIfError(err)
+	defer db.Close()
+
+	fmt.Printf("Stories created\n")
+
+	return nil
+}
+
+var initCmd = &cobra.Command{
+	Use:   "init",
+	Short: "Initilze stories. You need too create .nomsconfig",
+	RunE:  runInit,
+}
+
+func init() {
+	RootCmd.AddCommand(initCmd)
+}
