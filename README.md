bde2ae0b5733269441e3c1b6bf3d9f9b6a894db7
Alexis Bredel <alexis.bredel@outlook.com>
2020-05-11 14:55:39 +0200

    no duplicate create

--- README.md
+++ README.md
@@ -10,3 +10,7 @@ Model of datas :
 	    Effort int          // init 0
 	    State string        // Open or Close
     }
+
+The goal of this programme is to syncronize databases of stories in local machine.
+
+The StorieManager need a file ".nomsconfig" in the path of the executable. You can configure the programme with this file like default database or user.
--- commands/create_story.go
+++ commands/create_story.go
@@ -8,25 +8,36 @@ import (
 	"github.com/attic-labs/noms/go/d"
 	"github.com/attic-labs/noms/go/datas"
 	"github.com/attic-labs/noms/go/spec"
+	"github.com/attic-labs/noms/go/types"
 
 	"github.com/spf13/cobra"
 )
 
 func runCreateStory(cmd *cobra.Command, args []string) error {
 	ID := args[0]
+	title := args[1]
 	cfg := config.NewResolver() //config default db "Stories"
-	db, ds, err := cfg.GetDataset(ID)
+	db, err := cfg.GetDatabase("")
 	d.PanicIfError(err)
 	defer db.Close()
 
+	// Check no duplicate story
+	db.Datasets().IterAll(func(k, v types.Value) {
+		if fmt.Sprint(k) == ID {
+			d.CheckErrorNoUsage(errors.New(fmt.Sprintf("Error, ID : %s allready exist in database", ID)))
+		}
+	})
+
 	// Create
-	absPath := ApplyStructEdits(db, NewStory(ID), nil, nil)
+	absPath := ApplyStructEdits(db, NewStory(title), nil, nil)
 
 	// Commits
 	value := absPath.Resolve(db)
 	if value == nil {
 		d.CheckErrorNoUsage(errors.New(fmt.Sprintf("Error resolving value: %s", absPath.String())))
 	}
+
+	ds := db.GetDataset(ID)
 
 	oldCommitRef, oldCommitExists := ds.MaybeHeadRef()
 	if oldCommitExists {
@@ -46,9 +57,9 @@ func runCreateStory(cmd *cobra.Command, args []string) error {
 }
 
 var createStoryCmd = &cobra.Command{
-	Use:   "create <ID>",
+	Use:   "create <ID> <title>",
 	Short: "Create a new story.",
-	Args:  cobra.ExactArgs(1),
+	Args:  cobra.ExactArgs(2),
 	RunE:  runCreateStory,
 }
 
